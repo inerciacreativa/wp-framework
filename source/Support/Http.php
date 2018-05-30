d26314366d2db21750930a06629bb71ad56a8546
José Cuesta <jose.cuesta@inerciacreativa.com>
2018-05-30 03:30:05 +0200

    Code optimizations

--- source/Support/Http.php
+++ source/Support/Http.php
@@ -2,41 +2,41 @@
 
 namespace ic\Framework\Support;
 
-if (!defined('HTTP_URL_REPLACE')) {
-    define('HTTP_URL_REPLACE', 1);
+if (!\defined('HTTP_URL_REPLACE')) {
+    \define('HTTP_URL_REPLACE', 1);
 }
-if (!defined('HTTP_URL_JOIN_PATH')) {
-    define('HTTP_URL_JOIN_PATH', 2);
+if (!\defined('HTTP_URL_JOIN_PATH')) {
+    \define('HTTP_URL_JOIN_PATH', 2);
 }
-if (!defined('HTTP_URL_JOIN_QUERY')) {
-    define('HTTP_URL_JOIN_QUERY', 4);
+if (!\defined('HTTP_URL_JOIN_QUERY')) {
+    \define('HTTP_URL_JOIN_QUERY', 4);
 }
-if (!defined('HTTP_URL_STRIP_USER')) {
-    define('HTTP_URL_STRIP_USER', 8);
+if (!\defined('HTTP_URL_STRIP_USER')) {
+    \define('HTTP_URL_STRIP_USER', 8);
 }
-if (!defined('HTTP_URL_STRIP_PASS')) {
-    define('HTTP_URL_STRIP_PASS', 16);
+if (!\defined('HTTP_URL_STRIP_PASS')) {
+    \define('HTTP_URL_STRIP_PASS', 16);
 }
-if (!defined('HTTP_URL_STRIP_AUTH')) {
-    define('HTTP_URL_STRIP_AUTH', 32);
+if (!\defined('HTTP_URL_STRIP_AUTH')) {
+    \define('HTTP_URL_STRIP_AUTH', 32);
 }
-if (!defined('HTTP_URL_STRIP_PORT')) {
-    define('HTTP_URL_STRIP_PORT', 64);
+if (!\defined('HTTP_URL_STRIP_PORT')) {
+    \define('HTTP_URL_STRIP_PORT', 64);
 }
-if (!defined('HTTP_URL_STRIP_PATH')) {
-    define('HTTP_URL_STRIP_PATH', 128);
+if (!\defined('HTTP_URL_STRIP_PATH')) {
+    \define('HTTP_URL_STRIP_PATH', 128);
 }
-if (!defined('HTTP_URL_STRIP_QUERY')) {
-    define('HTTP_URL_STRIP_QUERY', 256);
+if (!\defined('HTTP_URL_STRIP_QUERY')) {
+    \define('HTTP_URL_STRIP_QUERY', 256);
 }
-if (!defined('HTTP_URL_STRIP_FRAGMENT')) {
-    define('HTTP_URL_STRIP_FRAGMENT', 512);
+if (!\defined('HTTP_URL_STRIP_FRAGMENT')) {
+    \define('HTTP_URL_STRIP_FRAGMENT', 512);
 }
-if (!defined('HTTP_URL_STRIP_ALL')) {
-    define('HTTP_URL_STRIP_ALL', 1024);
+if (!\defined('HTTP_URL_STRIP_ALL')) {
+    \define('HTTP_URL_STRIP_ALL', 1024);
 }
 
-if (!function_exists('http_build_url')) {
+if (!\function_exists('http_build_url')) {
     /**
      * Build a URL.
      *
