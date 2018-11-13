d034703a681cb5d8e6095b03b058335128829e44
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-11-13 12:01:56 +0100

    New loader to normalize paths

---
+++ ic-framework-loader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: ic Framework
+ * Plugin URI:  https://github.com/inerciacreativa/wp-framework
+ * Version:     2.0.9
+ * Text Domain: ic-framework
+ * Domain Path: /languages
+ * Description: Framework para la elaboración de plugins.
+ * Author:      Jose Cuesta
+ * Author URI:  https://inerciacreativa.com/
+ * License:     MIT
+ * License URI: https://opensource.org/licenses/MIT
+ */
+
+include_once __DIR__ . '/ic-framework/ic-framework.php';
