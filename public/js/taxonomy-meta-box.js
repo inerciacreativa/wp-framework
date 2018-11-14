718684dba6e81602da8b1f1080df5678d3ea7fe9
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-11-14 10:58:34 +0100

    Removed unnecessary fix for taxonomies metabox

--- public/js/taxonomy-meta-box.js
+++ public/js/taxonomy-meta-box.js
@@ -2,43 +2,45 @@
  * @param {jQuery} $ jQuery object.
  */
 (function ($) {
-    $('.ic-meta-box').each(function () {
-        var div = $(this),
-            id = div.attr('id'),
-            parts,
-            taxonomy,
-            terms;
+  $('.ic-meta-box').each(function () {
+    var div = $(this),
+      id = div.attr('id'),
+      parts,
+      taxonomy,
+      terms;
 
-        parts = id.split('-');
-        parts.shift();
-        taxonomy = parts.join('-');
-        terms = $('#' + taxonomy + '-all, #' + taxonomy + '-pop').find('input');
+    parts = id.split('-');
+    parts.shift();
+    taxonomy = parts.join('-');
+    terms = $('#' + taxonomy + '-all, #' + taxonomy + '-pop').find('input');
 
-        // Fix to create non hierarchical terms when multiple terms are allowed.
-        // Concatenates the values of the 'data-value' attributes of the inputs.
-        if (!div.hasClass('ic-meta-box-hierarchical')) {
-            $('#post').submit(function () {
-                var values = [],
-                    input = $('input[name="tax_input[' + taxonomy + ']"]');
+    // Fix to create non hierarchical terms when multiple terms are allowed.
+    // Concatenates the values of the 'data-value' attributes of the inputs.
+    /*
+  if (!div.hasClass('ic-meta-box-hierarchical')) {
+    $('#post').submit(function () {
+      var values = [],
+        input = $('input[name="tax_input[' + taxonomy + ']"]');
 
-                $('#' + taxonomy + '-all :checked').each(function () {
-                    values.push($(this).attr('data-value'));
-                });
+      $('#' + taxonomy + '-all :checked').each(function () {
+        values.push($(this).attr('data-value'));
+      });
 
-                input.val(values.join(','));
+      input.val(values.join(','));
 
-                return true;
-            });
-        }
-
-        // Fix to sync popular terms.
-        terms.on('click', function () {
-            var input = $(this),
-                id = input.val(),
-                checked = input.is(':checked');
-
-            terms.filter(':radio').prop('checked', false);
-            terms.filter('[value="' + id + '"]').prop('checked', checked);
-        });
+      return true;
     });
+  }
+  */
+
+    // Fix to sync popular terms.
+    terms.on('click', function () {
+      var input = $(this),
+        id = input.val(),
+        checked = input.is(':checked');
+
+      terms.filter(':radio').prop('checked', false);
+      terms.filter('[value="' + id + '"]').prop('checked', checked);
+    });
+  });
 })(jQuery);
