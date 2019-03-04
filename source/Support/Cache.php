66b949425cd0ae52daec7f0448777ae9946bc306
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2019-03-04 22:22:31 +0100

    Added type hints

--- source/Support/Cache.php
+++ source/Support/Cache.php
@@ -10,57 +10,59 @@ namespace ic\Framework\Support;
 class Cache
 {
 
-    /**
-     * @param string $name
-     * @param mixed  $value
-     * @param int    $expiration
-     */
-    public static function set($name, $value, $expiration = 0)
-    {
-        set_transient($name, $value, $expiration);
-    }
+	/**
+	 * @param string $name
+	 * @param mixed  $value
+	 * @param int    $expiration
+	 *
+	 * @return bool
+	 */
+	public static function set(string $name, $value, int $expiration = 0): bool
+	{
+		return set_transient($name, $value, $expiration);
+	}
 
-    /**
-     * @param string $name
-     *
-     * @return mixed
-     */
-    public static function get($name)
-    {
-        return get_transient($name);
-    }
+	/**
+	 * @param string $name
+	 *
+	 * @return mixed
+	 */
+	public static function get(string $name)
+	{
+		return get_transient($name);
+	}
 
-    /**
-     * @param string    $name
-     * @param mixed     $value
-     * @param null|bool $autoload
-     *
-     * @return bool
-     */
-    public static function push($name, $value, $autoload = null)
-    {
-        return update_option($name, $value, $autoload);
-    }
+	/**
+	 * @param string    $name
+	 * @param mixed     $value
+	 * @param null|bool $autoload
+	 *
+	 * @return bool
+	 */
+	public static function push(string $name, $value, bool $autoload = null): bool
+	{
+		return update_option($name, $value, $autoload);
+	}
 
-    /**
-     * @param string $name
-     * @param bool   $default
-     *
-     * @return mixed
-     */
-    public static function pull($name, $default = false)
-    {
-        return get_option($name, $default);
-    }
+	/**
+	 * @param string $name
+	 * @param mixed  $default
+	 *
+	 * @return mixed
+	 */
+	public static function pull(string $name, $default = false)
+	{
+		return get_option($name, $default);
+	}
 
-    /**
-     * @param string $name
-     *
-     * @return bool
-     */
-    public static function forget($name)
-    {
-        return delete_option($name);
-    }
+	/**
+	 * @param string $name
+	 *
+	 * @return bool
+	 */
+	public static function forget(string $name): bool
+	{
+		return delete_option($name);
+	}
 
 }
