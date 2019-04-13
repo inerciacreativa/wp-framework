ff344ed6ae131a902fffaf061ffcecf1d39c4c1a
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2019-04-13 05:51:22 +0200

    Use new Settings

--- source/Backend.php
+++ source/Backend.php
@@ -2,11 +2,10 @@
 
 namespace ic\Framework;
 
-use ic\Framework\Plugin\PluginClass;
-use ic\Framework\Settings\Settings;
-use ic\Framework\Settings\Form\Section;
-use ic\Framework\Settings\Form\Tab;
 use ic\Framework\Html\Tag;
+use ic\Framework\Plugin\PluginClass;
+use ic\Framework\Settings\Form\Section;
+use ic\Framework\Settings\Settings;
 
 /**
  * Class Backend
@@ -16,44 +15,29 @@ use ic\Framework\Html\Tag;
 class Backend extends PluginClass
 {
 
-    /**
-     * @inheritdoc
-     *            
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    protected function initialize(): void
-    {
-        Settings::siteOptions($this->id(), $this->getOptions(), $this->name())
-                ->addTab('video', function (Tab $tab) {
-                    $tab
-                        ->setTitle(__('Video Options', $this->id()))
-                        ->addSection('youtube', function (Section $section) {
-                            $section
-                                ->title(__('YouTube credentials', $this->id()))
-                                ->description(sprintf(
-                                    __('Required for using YouTube. Create a public API key in the %s and copy your API key.', $this->id()),
-                                    Tag::a(['href' => 'https://console.developers.google.com/'], 'Google Developers Console')
-                                ))
-                                ->text('youtube.credentials.key', __('API Key', $this->id()), [
-                                    'class' => 'regular-text',
-                                ]);
-                        })
-                        ->addSection('vimeo', function (Section $section) {
-                            $section
-                                ->title(__('Vimeo credentials', $this->id()))
-                                ->description(sprintf(
-                                    __('Required for using Vimeo. Register a new application in %s and copy your client identifier and secrets.', $this->id()),
-                                    Tag::a(['href' => 'https://developer.vimeo.com/api/start'], 'Vimeo Developer')
-                                ))
-                                ->text('vimeo.credentials.id', __('Client Identifier', $this->id()), [
-                                    'class' => 'regular-text',
-                                ])
-                                ->text('vimeo.credentials.secret', __('Client Secrets', $this->id()), [
-                                    'class' => 'regular-text',
-                                ]);
-                        });
-                });
-    }
+	/**
+	 * @inheritdoc
+	 */
+	protected function initialize(): void
+	{
+		Settings::siteOptions($this->getOptions(), $this->name())
+		        ->section('youtube', function (Section $section) {
+			        $section->title(__('YouTube credentials', $this->id()))
+			                ->description(sprintf(__('Required for using YouTube. Create a public API key in the %s and copy your API key.', $this->id()), Tag::a(['href' => 'https://console.developers.google.com/'], 'Google Developers Console')))
+			                ->text('youtube.credentials.key', __('API Key', $this->id()), [
+				                'class' => 'regular-text',
+			                ]);
+		        })
+		        ->section('vimeo', function (Section $section) {
+			        $section->title(__('Vimeo credentials', $this->id()))
+			                ->description(sprintf(__('Required for using Vimeo. Register a new application in %s and copy your client identifier and secrets.', $this->id()), Tag::a(['href' => 'https://developer.vimeo.com/api/start'], 'Vimeo Developer')))
+			                ->text('vimeo.credentials.id', __('Client Identifier', $this->id()), [
+				                'class' => 'regular-text',
+			                ])
+			                ->text('vimeo.credentials.secret', __('Client Secrets', $this->id()), [
+				                'class' => 'regular-text',
+			                ]);
+		        });
+	}
 
 }
