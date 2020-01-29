c7856f1390058ccf84391ebba6b82adf75368cc7
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2020-01-29 18:31:17 +0100

    Updated version

--- ic-framework-loader.php
+++ ic-framework-loader.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ic Framework
  * Plugin URI:  https://github.com/inerciacreativa/wp-framework
- * Version:     4.0.3
+ * Version:     4.0.4
  * Text Domain: ic-framework
  * Domain Path: /languages
  * Description: Framework para la elaboración de plugins.
